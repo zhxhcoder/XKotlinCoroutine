bca7f189aec686ea75bf448708496d622745154b
zhxh <zhxhcoder@gmail.com>
2019-05-30 11:47:20 +0800

    共享的可变状态与并发

---
+++ app/src/main/java/com/zhxh/coroutines/ui/GuideActivity.kt
@@ -0,0 +1,13 @@
+package com.zhxh.coroutines.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.zhxh.coroutines.R
+
+class GuideActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guide)
+    }
+}
